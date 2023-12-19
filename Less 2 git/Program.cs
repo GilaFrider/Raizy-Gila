7a30d74f062d6d6aabfca30bbd00157e2ca270b7
פרידר גילה < @mbjcomp.local>
2023-12-19 20:06:33 +0200

    Gila

--- Less 2 git/Program.cs
+++ Less 2 git/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+void Age(int age)
+{
+    Console.WriteLine("Yor age is:" + age );
+}
+String getString()
+{
+    String myString = "I ❤ myself";
+    return myString;
+}
+Console.WriteLine(getString());
